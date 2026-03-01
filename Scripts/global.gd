b6728c05e200279e2820bcd44b2ffadeeea6a6b5
Kevin Eleven <hiem.lhr@gmail.com>
2026-03-01 21:21:05 +0500

    Added fullscreen keybind

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -3,11 +3,19 @@ extends Node
 
 var world_gridmap: GridMap = null
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func  _ready() -> void:
+	get_viewport().get_window().mode = Window.MODE_FULLSCREEN
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("toggle_fullscreen"):
+		if get_viewport().get_window().mode == Window.MODE_FULLSCREEN:
+			get_viewport().get_window().mode = Window.MODE_WINDOWED
+			var ratio = Vector2(get_viewport().get_window().size) / Vector2(DisplayServer.screen_get_size())
+			var percent = (ratio.x + ratio.y) / 2
+			if percent > .75:
+				get_viewport().get_window().size.x /= 1.5
+				get_viewport().get_window().size.y /= 1.472
+			get_viewport().get_window().move_to_center()
+		else:
+			get_viewport().get_window().mode = Window.MODE_FULLSCREEN
