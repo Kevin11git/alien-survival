d65a7308736f5ea93ccebd9843eb2233938b6e91
Kevin Eleven <hiem.lhr@gmail.com>
2026-03-01 20:16:25 +0500

    (PHASE 1 COMPLETE!) Added block rotation with the R key before placing them

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -10,11 +10,19 @@ var selected_block: int = 0:
 	set(val):
 		selected_block = wrapi(val, 0, Global.world_gridmap.mesh_library.get_item_list().size())
 		update_selected_block()
+# Before placing a block it can be rotated
+# in the Y axis using the "rotate_block" action which is R by default
+var selected_rotation: float = 0: # (In degrees)
+	set(val):
+		selected_rotation = fposmod(val, 360.0)
+		%BlockPlacingPreview.rotation_degrees.y = selected_rotation
 
 func update_selected_block():
 	if not Global.world_gridmap:
 		printerr("Gridmap not set!")
 		return
+	
+	selected_rotation = 0 # Reset rotation
 	%BlockPlacingPreview.mesh = Global.world_gridmap.mesh_library.get_item_mesh(selected_block)
 	%SelectedHotbarItem.reparent(get_node("%HotbarItem" + str(selected_block + 1)), false)
 	
@@ -65,17 +73,7 @@ func _input(event: InputEvent) -> void:
 		%CameraAnchor.rotation_degrees.x += (-event.relative.y / 10) * sensitivity
 		%CameraAnchor.rotation_degrees.x = clampf(%CameraAnchor.rotation_degrees.x, -90.0, 90.0)
 		
-	# Select block from hotbar
-	# using number keys
-	if Input.is_physical_key_pressed(KEY_1):
-		selected_block = 0
-	if Input.is_physical_key_pressed(KEY_2):
-		selected_block = 1
-	if Input.is_physical_key_pressed(KEY_3):
-		selected_block = 2
-	if Input.is_physical_key_pressed(KEY_4):
-		selected_block = 3
-	# using scroll wheel
+	# Select block from hotbar using scroll wheel
 	if event is InputEventMouseButton and event.pressed:
 		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
 			selected_block += 1
@@ -84,6 +82,24 @@ func _input(event: InputEvent) -> void:
 
 
 func _process(delta: float) -> void:
+	# Select block from hotbar using number keys
+	if Input.is_physical_key_pressed(KEY_1):
+		selected_block = 0
+	if Input.is_physical_key_pressed(KEY_2):
+		selected_block = 1
+	if Input.is_physical_key_pressed(KEY_3):
+		selected_block = 2
+	if Input.is_physical_key_pressed(KEY_4):
+		selected_block = 3
+	
+	# rotate block to place
+	if Input.is_action_just_pressed("rotate_block"):
+		selected_rotation += 90
+	
+	# process block breaking, placing and showing %BlockPlacingPreview
+	process_block_interaction()
+
+func process_block_interaction():
 	var normal: Vector3            # The side of the block being looked at
 	var ray_hit_pos: Vector3       # The exact position of the collison in the ray
 	var target_block_pos: Vector3  # The center position of block being looked at
@@ -101,11 +117,15 @@ func _process(delta: float) -> void:
 		
 		# Placing and breaking block
 		if Input.is_action_just_pressed("place_block"):
-			Global.world_gridmap.set_cell_item(Global.world_gridmap.local_to_map(placing_block_pos), selected_block)
+			Global.world_gridmap.set_cell_item(Global.world_gridmap.local_to_map(placing_block_pos), selected_block, rotation_to_gridmap_orientation(selected_rotation))
 			#print(str(%BlockPlaceCheckArea3D.has_overlapping_bodies()) + ", " + str(%BlockPlaceCheckArea3D.has_overlapping_areas()))
 		
 		if Input.is_action_just_pressed("break_block"):
 			Global.world_gridmap.set_cell_item(Global.world_gridmap.local_to_map(target_block_pos), -1)
 	else:
 		%BlockPlacingPreview.hide()
-	
+
+func rotation_to_gridmap_orientation(angle_degrees: float, axis: Vector3 = Vector3.UP):
+	var toQuaternion: Quaternion = Quaternion(axis, deg_to_rad(angle_degrees))
+	var cell_item_orientation: int = Global.world_gridmap.get_orthogonal_index_from_basis(Basis(toQuaternion))
+	return cell_item_orientation
